5fd582b80e69eb6bea80890ed2588a296ef17de7
Matsura Yuma <41217985+sabigara@users.noreply.github.com>
2023-02-20 10:54:07 +0900

    Support theme switch and search

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,8 +1,13 @@
 import Fuse from "fuse.js";
+import { TextInput } from "@camome/core/TextInput";
+import { IconSearch } from "@tabler/icons-react";
 import { useEffect, useRef, useState } from "react";
 import Card from "@components/Card";
 import slugify from "@utils/slugify";
 import type { BlogFrontmatter } from "@content/_schemas";
+import { TextInputGroup } from "@camome/core/TextInputGroup";
+import styles from "./Search.module.scss";
+import clsx from "clsx";
 
 export type SearchItem = {
   title: string;
@@ -70,40 +75,35 @@ export default function SearchBar({ searchList }: Props) {
   }, [inputVal]);
 
   return (
-    <>
-      <label className="relative block">
-        <span className="absolute inset-y-0 left-0 flex items-center pl-2 opacity-75">
-          <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-            <path d="M19.023 16.977a35.13 35.13 0 0 1-1.367-1.384c-.372-.378-.596-.653-.596-.653l-2.8-1.337A6.962 6.962 0 0 0 16 9c0-3.859-3.14-7-7-7S2 5.141 2 9s3.14 7 7 7c1.763 0 3.37-.66 4.603-1.739l1.337 2.8s.275.224.653.596c.387.363.896.854 1.384 1.367l1.358 1.392.604.646 2.121-2.121-.646-.604c-.379-.372-.885-.866-1.391-1.36zM9 14c-2.757 0-5-2.243-5-5s2.243-5 5-5 5 2.243 5 5-2.243 5-5 5z"></path>
-          </svg>
-        </span>
-        <input
-          className="block w-full rounded border border-skin-fill 
-        border-opacity-40 bg-skin-fill py-3 pl-10
-        pr-3 placeholder:italic placeholder:text-opacity-75 
-        focus:border-skin-accent focus:outline-none"
-          placeholder="Search for anything..."
-          type="text"
-          name="search"
-          value={inputVal}
-          onChange={handleChange}
-          autoComplete="off"
-          autoFocus
-          ref={inputRef}
-        />
-      </label>
+    <div className={clsx(styles.container, "stack-6")}>
+      <TextInputGroup
+        startDecorator={<IconSearch />}
+        input={
+          <TextInput
+            type="search"
+            placeholder="Search"
+            fill
+            value={inputVal}
+            onChange={handleChange}
+            autoComplete="off"
+            autoFocus
+            ref={inputRef}
+          />
+        }
+        className={styles["input-group"]}
+      />
 
       {inputVal.length > 1 && (
-        <div className="mt-8">
+        <div className="">
           Found {searchResults?.length}
           {searchResults?.length && searchResults?.length === 1
             ? " result"
             : " results"}{" "}
-          for '{inputVal}'
+          for "{inputVal}"
         </div>
       )}
 
-      <ul>
+      <ul className="stack">
         {searchResults &&
           searchResults.map(({ item, refIndex }) => (
             <Card
@@ -113,6 +113,6 @@ export default function SearchBar({ searchList }: Props) {
             />
           ))}
       </ul>
-    </>
+    </div>
   );
 }
